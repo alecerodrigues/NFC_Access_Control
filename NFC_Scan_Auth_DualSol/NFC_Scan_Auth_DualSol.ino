42e57328df7443a730ba1de2ee35188c6eeb5ab8
Alec Rodrigues <57117759+alecerodrigues@users.noreply.github.com>
2021-04-01 19:25:48 -0400

    Add files via upload

---
+++ NFC_Scan_Auth_DualSol/NFC_Scan_Auth_DualSol.ino
@@ -0,0 +1,67 @@
+//NFC
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+int solenoidPinA = 3;
+int solenoidPinB = 4;
+
+void setup()
+{
+  pinMode(solenoidPinA, OUTPUT);   //Sets that pin as an output
+  pinMode(solenoidPinB, OUTPUT);   //Sets that pin as an output
+
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+
+}
+void loop()
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "04 15 91 7A 8B A0 25") //UID of the access card (4x HEX = RFID, 6x HEX = NFC)
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(solenoidPinA, HIGH);
+    digitalWrite(solenoidPinB, HIGH);      //Switch Solenoid ON
+    delay(1000);                          //Wait 1 Second
+    digitalWrite(solenoidPinA, LOW);       //Switch Solenoid OFF
+    digitalWrite(solenoidPinB, LOW);
+    delay(1000);                          //Wait 1 Second
+  }
+
+  else   {
+    Serial.println(" Access denied");
+    delay(500);
+  }
+}
